7cba122ce65ab410f945fa0bcaaf3840721eebf6
emilino <emilino@pinpong.com>
2022-08-31 19:34:44 -0300

    agrego data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
 import Header from './components/header/Header';
 import Formulario from './components/formulario/Formulario';
+import { useState } from 'react';
 
+const DUMMIE_EXPENSES_DATA = [
+  {
+    id: 1,
+    seccion: "Materiales",
+    pagoProv: "Codimat",
+    etapDescr: "Cemento y cal",
+    amount: 15000,
+    date: new Date(2022,8,31)
+  },
+  {
+    id: 2,
+    seccion: "Mano de Obra",
+    pagoProv: "Julio",
+    etapDescr: "1",
+    amount: 40000,
+    date: new Date(2022,8,31)
+  }
+]
 
 function App() {
 
-  const saveDataHandler = (data) => {
-    console.log(data);
+  const [expenses, setExpenses]=useState(DUMMIE_EXPENSES_DATA);
+
+  const saveDataHandler = (newExpenseData) => {
+    
+    setExpenses ( (prevExpenses) => {
+      return [newExpenseData, ...prevExpenses];
+    });
+
+    console.log(expenses);
   }
+
+
 
   return (
     <div>
