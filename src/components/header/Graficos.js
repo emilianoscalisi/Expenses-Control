8eed344696a7463af6442d2a396a03936acea2a9
Emiliano Scalisi <emilianoscalisi1986@gmail.com>
2022-12-01 11:51:47 -0300

    cambio contenido grafico

--- src/components/header/Graficos.js
+++ src/components/header/Graficos.js
@@ -47,18 +47,15 @@ const Graficos = (props) => {
                 onClose={handleClose}
                 aria-describedby="alert-dialog-slide-description"
             >
-                <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+                <DialogTitle>{"Gráficos Resumen"}</DialogTitle>
                 <DialogContent>
-                    <DialogContentText id="alert-dialog-slide-description">
-                        Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.
-                    </DialogContentText>
+                    {/* <DialogContentText id="alert-dialog-slide-description">
+                        
+                    </DialogContentText> */}
+
+                    <div>
+                        
+                    </div>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cerrar</Button>
